5443a152b80dc05851248d868a1d990bbdb0cd74
Nithika14 <madhugopal2710@gmail.com>
2026-03-18 13:59:05 +0530

    Portfolio

--- app.js
+++ app.js
@@ -6,6 +6,14 @@ document.querySelectorAll('.card').forEach(card=>{
     });
 });
 
+document.querySelector(".contact-btn").addEventListener("click", function () {
+    const section = document.getElementById("contact");
+    const yOffset = -80; // adjust based on header height
+    const y = section.getBoundingClientRect().top + window.pageYOffset + yOffset;
+
+    window.scrollTo({ top: y, behavior: "smooth" });
+});
+
 const video1 = document.getElementById('projectVideo1');
 const video2 = document.getElementById('projectVideo2');
 const video3 = document.getElementById('projectVideo3');
@@ -24,13 +32,50 @@ videoList.forEach(function(video){
 
 successMessage = document.getElementById('successMessage');
 
-document.getElementById('contactForm').addEventListener('submit', function(e){
+// document.getElementById('contactForm').addEventListener('submit', function(e){
+//     e.preventDefault();
+//     document.getElementById('successMessage').style.display = 'block';
+
+//     this.reset();
+
+//     setTimeout(function(){
+//         document.getElementById('successMessage').style.display = 'none';
+//     }, 3000);
+// })
+
+// Initialize EmailJS
+(function () {
+    emailjs.init("DhwWo9pI02QffAMcK"); // replace this
+})();
+
+// Form Submit
+document.getElementById("contactForm").addEventListener("submit", function (e) {
     e.preventDefault();
-    document.getElementById('successMessage').style.display = 'block';
 
-    this.reset();
+    const btn = document.getElementById("submitBtn");
+    btn.innerText = "Sending...";
 
-    setTimeout(function(){
-        document.getElementById('successMessage').style.display = 'none';
-    }, 3000);
-})
+    emailjs.send("service_o17oc9j", "template_paf3e3o", {
+        name: document.getElementById("name").value,
+        email: document.getElementById("email").value,
+        subject: document.getElementById("subject").value,
+        message: document.getElementById("message").value
+    })
+    .then(() => {
+        btn.innerText = "SEND MESSAGE";
+
+        document.getElementById("successMessage").style.display = "block";
+
+        document.getElementById("contactForm").reset();
+
+        // Hide message after 4 sec
+        setTimeout(() => {
+            document.getElementById("successMessage").style.display = "none";
+        }, 4000);
+    })
+    .catch((error) => {
+        btn.innerText = "SEND MESSAGE";
+        alert("Failed to send message !!");
+        console.log(error);
+    });
+});
